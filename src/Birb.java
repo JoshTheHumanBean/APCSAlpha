132f3dfb69f74cf6bd253ef71cf7f811d1c7f48e
Josh The Human <47869156+JoshTheHumanBean@users.noreply.github.com>
2021-12-14 11:55:23 -0600

    Added more methods and attributes

--- src/Birb.java
+++ src/Birb.java
@@ -1,3 +1,32 @@
 public class Birb {
+    private String politicalAffiliation;
+    private String religiousAffiliation;
+    private String color;
+    private String age;
+    private String species;
+    private int flightSpeed;
+    private int groundSpeed;
+    private boolean whenActive;
+
+    public void printInfo(){
+        System.out.printf("Political Affiliation: %s%nReligious Affiliation: %s%n" +
+                "Color: %s%nAge: %s%nHighest Observed Flight Speed: %s%n" +
+                "Highest Observed Ground Speed: %s%nTime of Day When Active: %s%n",
+                politicalAffiliation, religiousAffiliation, color, flightSpeed, groundSpeed, whenBirdActive());
+    }
+
+    public void calculateFlightSpeed(int distance, int time){
+        if (distance/time >= 0 && distance/time > flightSpeed){flightSpeed = distance/time;}
+    }
+
+    public String whenBirdActive(){
+        String string;
+        if(whenActive = true){string = "Day";}
+
+        else {string = "Night";}
+
+        return string;
+    }
+
 
 }
